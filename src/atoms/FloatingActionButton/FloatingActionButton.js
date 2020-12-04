4253b2cf8ca023168165a0f4e7b09a062cdd61a3
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-12-04 13:49:45 +0100

    added FloatingActionButton component updated Note and NotesPage components

---
+++ src/atoms/FloatingActionButton/FloatingActionButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './FloatingActionButton.module.css';
+
+const floatingActionButton = () => (
+  <button className={classes.FloatingActionButton}>+</button>
+);
+
+export default floatingActionButton;
--- src/pages/NotesPage/NotesPage.js
+++ src/pages/NotesPage/NotesPage.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 
 import Layout from '../../templates/Layout/Layout';
 import Notes from '../../organisms/Notes/Notes';
+import FloatingActionButton from '../../atoms/FloatingActionButton/FloatingActionButton';
 
 class NotesPage extends Component{
   render(){
     return(
       <Layout>
         <Notes />
+        <FloatingActionButton />
       </Layout>
     )
   }
