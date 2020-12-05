30b02bbdcb15e5885ccb54126d2308b2e41e15fe
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-12-05 22:34:00 +0100

    added font awesome and updated toolbar

--- src/organisms/Toolbar/Toolbar.js
+++ src/organisms/Toolbar/Toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => {
   return(
     <header className={classes.Toolbar}>
-      <div>Logo</div>
+      <FontAwesomeIcon className={classes.HamburgerMenu} icon={faBars} size="lg" />
+      <h1 className={classes.AppTitle}>Notes</h1>
       <nav>Navigation Items</nav>
     </header>
   );
