b8158f0d9a4b43e74ac374a0fe751d3c760d9a7f
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-12-07 11:52:02 +0100

    added onChange handler in NotePage component

--- src/pages/NotePage/NotePage.js
+++ src/pages/NotePage/NotePage.js
@@ -3,13 +3,26 @@ import React, {Component} from 'react';
 import Layout from '../../templates/Layout/Layout';
 import Input from '../../atoms/Input/Input';
 
-class NotePage extends Component{
+class NotePage extends Component {
+
+  state = {
+    title: 'Title',
+    description: 'Description'
+  }
+
+  onChangeHandler = (e, type) => {
+    if(type === 'title'){
+      this.setState({title: e.target.value});
+    }else{
+      this.setState({description: e.target.value});
+    }
+  }
 
   render(){
     return(
       <Layout>
-        <Input type='title' value='Title'/>
-        <Input type='description' value='Description'/>
+        <Input type='title' value={this.state.title} onChange={this.onChangeHandler}/>
+        <Input type='description' value={this.state.description} onChange={this.onChangeHandler}/>
       </Layout>
     );
   }
