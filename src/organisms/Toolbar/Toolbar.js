a5d96937127544198b453ad1de89bf5769652510
Aleksandar Vasilevski <vasilevski85@gmail.com>
2020-12-17 12:22:40 +0100

    removed Toolbar title, added NavigationItems logic in Toolbar

--- src/organisms/Toolbar/Toolbar.js
+++ src/organisms/Toolbar/Toolbar.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import classes from './Toolbar.module.css';
 
 const toolbar = (props) => {
+
+  const navigationItems = () => {
+    console.log(props.location.pathname);
+    switch(props.location.pathname){
+      case '/note':
+        return 'Note';
+      default:
+        return 'Notes'
+    }
+  }
+
   return(
     <header className={classes.Toolbar}>
       <FontAwesomeIcon className={classes.HamburgerMenu} icon={faBars} size="lg" />
-      <h1 className={classes.AppTitle}>Notes</h1>
-      <nav className={classes.NavigationItems}>Navigation Items</nav>
+      <nav className={classes.NavigationItems}>{navigationItems()}</nav>
     </header>
   );
 }
 
-export default toolbar;
+export default withRouter(toolbar);
