4a61b15f281065f1d22449ed30f08276e7de23d5
Aleksandar Vasilevski <vasilevski85@gmail.com>
2021-01-04 12:24:10 +0100

    note id is now created in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,7 +6,16 @@ const reducer = (state = initialState, action) => {
   if(action.type === 'CREATE'){
     return{
       ...state,
-      notes: [...state.notes, action.note]
+      // notes: [...state.notes, action.note]
+      notes: state.notes.concat({id: state.notes.length, title: action.note.title, description: action.note.description, timeStamp: action.note.timeStamp})
+    }
+  }else if(action.type === 'EDIT'){
+    return{
+
+    }
+  }else if(action.type === 'DELETE'){
+    return{
+
     }
   }
   return state;
